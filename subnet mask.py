455ccad7390760bd6a6bc7fe38c183d3bf96c2f2
Razor <103508415+sppu69@users.noreply.github.com>
2023-10-27 11:33:20 +0530

    Update subnet mask.py

--- subnet mask.py
+++ subnet mask.py
@@ -1,11 +1,42 @@
-ip = input("Enter IP address (e.g., 192.168.1.1): ")
-prefix_length = int(input("Enter prefix length (e.g., 24 for /24 subnet): "))
+def is_valid_IPv4(ip):
+    segments = ip.split('.')
+    if len(segments)!= 4:
+        return False
+    
+    for segment in segments:
+        if not segment.isdigit():
+            return False
+        if not (0 <= int(segment) <= 255):
+            return False
+        
+    return True
 
-# Calculate subnet mask
-subnet_mask = (1 << 32 - prefix_length) - 1
-subnet_mask = (subnet_mask << prefix_length) & 0xFFFFFFFF
-
-# Format the subnet mask in decimal format
-subnet_mask = ".".join(str((subnet_mask >> i) & 0xFF) for i in [24, 16, 8, 0])
-
-print("Subnet Mask:", subnet_mask)
+print("SUBNETTING AND SUBNET MASK")
+IP = input("Enter IP Address: ")
+if is_valid_IPv4(IP):
+    IP_components = IP.split('.')
+    class_check = int(IP_components[0])
+    mask = ""
+    if(class_check>0):
+        if(class_check<=127):
+            mask = "255.0.0.0"
+            print("Class A IP Address")
+            print("SUBNET MASK: ",mask)
+        elif(128<=class_check<=191):
+            mask = "255.255.0.0"
+            print("Class B IP Address")
+            print("SUBNET MASK: ",mask)
+        elif(192<=class_check<=223):
+            mask = "255.255.255.0"
+            print("CLass C IP Address")
+            print("SUBNET MASK: ",mask)
+        elif(224<=class_check<=239):
+            mask = "255.0.0.0"
+            print("Class D IP Address")
+            print("SUBNET MASK: ",mask)
+        elif(240<=class_check<=254):
+            mask = "255.0.0.0"
+            print("Class E IP Address")
+            print("SUBNET MASK: ",mask)
+else:
+    print("Invalid IP Address")
